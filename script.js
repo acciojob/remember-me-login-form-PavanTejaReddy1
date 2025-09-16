d8073723f56d1f04ea07462f22a3c3066f376ed4
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-16 23:21:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+const form = document.querySelector("form");
+const existing = document.getElementById("existing");
+
+form.addEventListener("submit", submitValues);
+
+function submitValues(e){
+	e.preventDefault();
+	
+	const name = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	const checkBox = document.getElementById("checkbox").checked;
+
+	if (checkBox) {
+		localStorage.setItem("name", name);
+		localStorage.setItem("password", password);
+	} else {
+		localStorage.clear();
+	}
+
+	alert(`Logged in as ${name}`);
+
+	e.target.reset();
+}
+
+if (localStorage.getItem("name")) {
+	existing.style.display = "block";
+	form.style.display = "none";
+	existing.addEventListener("submit", ()=>{
+		alert(`Logged in as ${localStorage.getItem("name")}`);
+	})
+}
+
+
+
+
+
+
+
+
